486eb277584de85b0890e46cdf52da92e3075ddd
tafajoel1-create <tafajoel1@gmail.com>
2026-06-06 18:46:11 +0100

    Create config.py for configuration settings

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+J.A.R.V.I.S Configuration Settings
+"""
+
+# AI Assistant Name
+AI_NAME = "J.A.R.V.I.S"
+AI_FULL_NAME = "Just A Rather Very Intelligent System"
+
+# Greeting settings
+FORMAL_MODE = True  # If True, uses formal greetings
+
+# Response settings
+DEBUG_MODE = False  # If True, prints debug information
